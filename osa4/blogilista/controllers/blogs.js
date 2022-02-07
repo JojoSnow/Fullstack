f8cd1ddc9b4019b253d8419a1d417c353c5df022
JojoSnow <milzwow94@gmail.com>
2022-02-07 10:54:32 +0200

    osa4d 4.21 done

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -53,8 +53,13 @@ blogsRouter.post('/', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
-	await Blog.findByIdAndRemove(request.params.id)
-	response.status(204).end()
+	const decodedToken = jwt.verify(request.token, process.env.SECRET)
+	const blog = await Blog.findById(request.params.id)
+
+	if (decodedToken.id.toString() === blog.user.toString()) {
+		await Blog.findByIdAndRemove(blog.id)
+		response.status(204).end()
+	}
 })
 
 blogsRouter.put('/:id', async (request, response, next) => {
