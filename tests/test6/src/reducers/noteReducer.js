06fbd35a6684a7965850391874c5f036432c5200
JojoSnow <milzwow94@gmail.com>
2022-02-23 11:44:34 +0200

    osa6b before redux toolkit

--- tests/test6/src/reducers/noteReducer.js
+++ tests/test6/src/reducers/noteReducer.js
@@ -1,4 +1,19 @@
-const noteReducer = (state = [], action) => {
+const initialState = [
+	{
+		content: 'reducer defines how redux store works',
+		important: true,
+		id: 1
+	},
+	{
+		content: 'state of store can contain any data',
+		important: false,
+		id: 2
+	}
+]
+
+const noteReducer = (state = initialState, action) => {
+	console.log('ACTION: ', action)
+
 	switch(action.type) {
 		case 'NEW_NOTE':
 			return [...state, action.data]
@@ -7,7 +22,7 @@ const noteReducer = (state = [], action) => {
 			const noteToChange = state.find(n => n.id === id)
 			const changedNote = {
 				...noteToChange,
-				import: !noteToChange.important
+				important: !noteToChange.important
 			}
 			return state.map(note => 
 					note.id !== id ? note : changedNote
