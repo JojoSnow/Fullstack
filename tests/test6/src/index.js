ed9ba01162adaa795d9c269cdee735cc04ba80de
JojoSnow <milzwow94@gmail.com>
2022-02-23 12:09:26 +0200

    osa6b test done

--- tests/test6/src/index.js
+++ tests/test6/src/index.js
@@ -1,23 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {createStore, combineReducers} from 'redux'
+import {configureStore} from '@reduxjs/toolkit'
 import {Provider} from 'react-redux'
 import App from './App'
+
 import noteReducer from './reducers/noteReducer'
 import filterReducer from './reducers/filterReducer'
-import {createNote} from './reducers/noteReducer'
-import {filterChange} from './reducers/filterReducer'
 
-const reducer = combineReducers({
-	notes: noteReducer,
-	filter: filterReducer
+const store = configureStore({
+	reducer: {
+		notes: noteReducer,
+		filter: filterReducer
+	}
 })
 
-const store = createStore(reducer)
-
-store.subscribe(() => console.log(store.getState()))
-store.dispatch(filterChange('IMPORTANT'))
-store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
+console.log(store.getState())
 
 ReactDOM.render(
 	<Provider store={store}>
