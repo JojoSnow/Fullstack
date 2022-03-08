15a9149f6823f768f3c89a77843333c39771a8ae
JojoSnow <milzwow94@gmail.com>
2022-03-08 13:49:58 +0200

    osa7b 7.7 start

---
+++ osa7/country-hook/src/App.js
@@ -0,0 +1,68 @@
+import {useState, useEffect} from 'react'
+import axios from 'axios'
+
+const useField = (type) => {
+	const [value, setValue] = useState('')
+
+	const onChange = (event) => {
+		setValue(event.target.value)
+	}
+	
+	return {
+		type, value, onChange
+	}
+}
+
+const useCountry = (name) => {
+	const [country, setCountry] = useState(null)
+	
+	useEffect(() => {
+		axios
+			.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+			.then(response => {
+				setCountry(response.data[0])
+		})
+	}, [name])
+
+	if (country) {
+		return country
+	}
+}
+
+const Country = ({country}) => {
+	if (!country) {
+		return <div>not found...</div>
+	}
+
+	return (
+		<div>
+			<h3>{country.name.common}</h3>
+			<div>Population: {country.population}</div>
+			<div>Capital: {country.capital}</div>
+			<img src={country.flags.png} height="100" alt={`flag of ${country.name.common}`} />
+		</div>
+	)
+}
+
+function App() {
+	const nameInput = useField('text')
+	const [name, setName] = useState('')
+	const country = useCountry(name)
+	const fetch = (e) => {
+		e.preventDefault()
+		setName(nameInput.value)
+	}
+
+	return (
+		<div>
+			<form onSubmit={fetch}>
+				<input {...nameInput} />
+				<button type="submit">Find</button>	
+			</form>
+
+			<Country country={country} />
+		</div>
+	)
+}
+
+export default App
