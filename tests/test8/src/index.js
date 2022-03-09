54fa1b0997ef48ce4063d8eb735822e6dd47b23e
JojoSnow <milzwow94@gmail.com>
2022-03-09 13:47:09 +0200

    osa7d test done

---
+++ tests/test8/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import 'core-js/stable/index.js'
+import 'regenerator-runtime/runtime.js'
+import './index.css'
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
