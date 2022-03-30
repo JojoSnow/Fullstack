a80b1a29f70848d764a1f321fb6f21dd02470d01
JojoSnow <milzwow94@gmail.com>
2022-03-30 11:00:52 +0300

    osa8e test done

--- tests/test10/models/person.js
+++ tests/test10/models/person.js
@@ -19,7 +19,13 @@ const personSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 		minlength: 3
-	}
+	},
+	friendOf: [
+		{
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User'
+		}
+	]
 })
 
 module.exports = mongoose.model('Person', personSchema)
---
+++ tests/test10/schema.js
@@ -0,0 +1,67 @@
+const {gql} = require('apollo-server')
+
+const typeDefs = gql`
+	type Address {
+		street: String!
+		city: String!
+	}
+
+	type Person {
+		name: String!
+		phone: String
+		address: Address!
+		friendOf: [User!]!
+		id: ID!
+	}
+
+	enum YesNo {
+		YES
+		NO
+	}
+
+	type User {
+		username: String!
+		friends: [Person!]!
+		id: ID!
+	}
+
+	type Token {
+		value: String!
+	}
+
+	type Query {
+		personCount: Int!
+		allPersons(phone: YesNo): [Person!]!
+		findPerson(name: String!): Person
+		me: User
+	}
+
+	type Subscription {
+		personAdded: Person!
+	}
+
+	type Mutation {
+		addPerson(
+			name: String!
+			phone: String
+			street: String!
+			city: String!
+		): Person
+		editNumber(
+			name: String!
+			phone: String!
+		): Person
+		createUser(
+			username: String!
+		): User
+		login(
+			username: String!
+			password: String!
+		): Token
+		addAsFriend(
+			name: String!
+		): User
+	}
+`
+
+module.exports = typeDefs
