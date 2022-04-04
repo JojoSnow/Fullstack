c5182e244faddc6701d2e756ad9db5d21a11339a
JojoSnow <milzwow94@gmail.com>
2022-04-04 11:24:14 +0300

    osa9b test 2/3 done

--- tests/test12/index.ts
+++ tests/test12/index.ts
@@ -1,29 +1,12 @@
-type Operation = 'multiply' | 'add' | 'divide';
+import express from 'express';
+const app = express();
 
-type Result = number;
+app.get('/ping', (_req, res) => {
+	res.send('pong');
+})
 
-const calculator = (a: number, b: number, op: Operation): Result => {
-	switch(op) {
-		case 'multiply':
-			return a * b;
-		case 'divide':
-			if (b === 0) throw new Error(`Can\'t divide by 0!`);
-			return a / b;
-		case 'add':
-			return a + b;
-		default:
-			throw new Error('Operation is not multiply, add or divide!');
-	}
-}
+const PORT = 3003;
 
-try {
-	console.log(calculator(1, 5, 'divide'));
-} catch (error: unknown) {
-	let errorMessage = 'Something went wrong.';
-	if (error instanceof Error) {
-		errorMessage += ' Error: ' + error.message;
-	}
-	console.log(errorMessage)
-}
-
-console.log(process.argv)
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`)
+})
