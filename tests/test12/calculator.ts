f29fc05b74ae15acdcb1d74d27ac101418afb570
JojoSnow <milzwow94@gmail.com>
2022-04-05 09:59:55 +0300

    osa9b test 3/3 done

--- tests/test12/calculator.ts
+++ tests/test12/calculator.ts
@@ -2,19 +2,19 @@ type Operation = 'multiply' | 'add' | 'divide';
 
 type Result = number;
 
-const calculator = (a: number, b: number, op: Operation): Result => {
+export const calculator = (a: number, b: number, op: Operation): Result => {
 	switch(op) {
 		case 'multiply':
 			return a * b;
 		case 'divide':
-			if (b === 0) throw new Error(`Can\'t divide by 0!`);
+			if (b === 0) throw new Error(`Can't divide by 0!`);
 			return a / b;
 		case 'add':
 			return a + b;
 		default:
 			throw new Error('Operation is not multiply, add or divide!');
 	}
-}
+};
 
 try {
 	console.log(calculator(1, 5, 'divide'));
@@ -23,7 +23,7 @@ try {
 	if (error instanceof Error) {
 		errorMessage += ' Error: ' + error.message;
 	}
-	console.log(errorMessage)
+	console.log(errorMessage);
 }
 
-console.log(process.argv)
+console.log(process.argv);
