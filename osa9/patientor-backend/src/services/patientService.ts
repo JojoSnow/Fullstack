07d1221dd2cdbb948304a32cdcaa475324d739a7
JojoSnow <milzwow94@gmail.com>
2022-04-06 13:06:41 +0300

    osa9c 9.11 done

---
+++ osa9/patientor-backend/src/services/patientService.ts
@@ -0,0 +1,17 @@
+import patients from '../../data/patients';
+
+import {NonSensitivePatients} from '../types';
+
+const getNonSensitivePatients = (): NonSensitivePatients[] => {
+	return patients.map(({id, name, dateOfBirth, gender, occupation}) => ({
+		id,
+		name,
+		dateOfBirth,
+		gender,
+		occupation
+	}));
+};
+
+export default {
+	getNonSensitivePatients
+};
--- osa9/patientor-backend/src/types.ts
+++ osa9/patientor-backend/src/types.ts
@@ -2,4 +2,15 @@ export interface Diagnose {
 	code: string;
 	name: string;
 	latin?: string;
-}
+};
+
+export interface Patients {
+	id: string,
+	name: string,
+	dateOfBirth: string,
+	ssn: string,
+	gender: string,
+	occupation: string
+};
+
+export type NonSensitivePatients = Omit<Patients, 'ssn'>;
