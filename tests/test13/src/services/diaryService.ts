9a6b8890f1f9f460c8608dedc52e6872a02750a8
JojoSnow <milzwow94@gmail.com>
2022-04-07 09:26:13 +0300

    osa9c test 3/3 done

--- tests/test13/src/services/diaryService.ts
+++ tests/test13/src/services/diaryService.ts
@@ -1,5 +1,9 @@
-import diaries from '../../data/diaries';
-import {NonSensitiveDiaryEntry, DiaryEntry} from '../types';
+import diaries from '../../data/diaryEntries';
+import {
+	NonSensitiveDiaryEntry,
+	DiaryEntry,
+	NewDiaryEntry
+} from '../types';
 
 const getEntries = (): Array<DiaryEntry> => {
 	return diaries;
@@ -12,14 +16,26 @@ const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
 		weather,
 		visibility
 	}));
-}
+};
 
-const addDiary = () => {
-	return null;
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+	const newDiaryEntry = {
+		id: Math.max(...diaries.map(d => d.id)) + 1,
+		...entry
+	};
+
+	diaries.push(newDiaryEntry);
+	return newDiaryEntry;
+};
+
+const findById = (id: number): DiaryEntry | undefined => {
+	const entry = diaries.find(d => d.id === id);
+	return entry;
 };
 
 export default {
 	getEntries,
 	getNonSensitiveEntries,
-	addDiary
+	addDiary,
+	findById
 };
