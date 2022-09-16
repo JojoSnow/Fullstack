e12dae4f33a0d554315b42aaf092575b3e7b75c2
JojoSnow <milzwow94@gmail.com>
2022-09-16 14:03:42 +0300

    osa13 13.3 done

--- osa13/blog-backend-app/index.js
+++ osa13/blog-backend-app/index.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { Sequelize } = require('sequelize');
+const { Sequelize, QueryTypes} = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
 	dialectOptions: {
@@ -13,7 +13,10 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
 const main = async () => {
 	try {
 		await sequelize.authenticate();
-		console.log('Connection has been established successfully.');
+		const blogs = await sequelize.query("SELECT * FROM blogs", {
+			type: QueryTypes.SELECT })
+		console.log(`${blogs[0].author}: ${blogs[0].title}, ${blogs[0].likes} likes`);
+		console.log(`${blogs[1].author}: ${blogs[1].title}, ${blogs[1].likes} likes`);
 		sequelize.close();
   	} catch (error) {
    		console.error('Unable to connect to the database:', error);
