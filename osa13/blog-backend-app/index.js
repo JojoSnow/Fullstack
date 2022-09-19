fd56f0979b4709168379026ac6c96c2d7c40644d
JojoSnow <milzwow94@gmail.com>
2022-09-19 12:14:50 +0300

    osa13b 13.5 done

--- osa13/blog-backend-app/index.js
+++ osa13/blog-backend-app/index.js
@@ -1,76 +1,20 @@
-require('dotenv').config();
-const { Sequelize, DataTypes, Model } = require('sequelize');
 const express = require('express');
 const app = express();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-	dialectOptions: {
-		ssl: {
-			require: true,
-			rejectUnauthorized: false
-		}
-	},
-});
+const { PORT } = require('./utils/config');
+const { connectToDatabase } = require('./utils/db');
 
-class Blog extends Model {}
+const blogsRouter = require('./controllers/blogs');
 
-Blog.init({
-	id: {
-		type: DataTypes.INTEGER,
-		primaryKey: true,
-		autoIncrement: true
-	},
-	author: {
-		type: DataTypes.TEXT,
-	},
-	url: {
-		type: DataTypes.TEXT,
-		allowNull: false
-	},
-	title: {
-		type: DataTypes.TEXT,
-		allowNull: false
-	},
-	likes: {
-		type: DataTypes.INTEGER
-	}
-}, {
-	sequelize,
-	underscored: true,
-	timestamps: false,
-	modelName: 'blog'
-});
+app.use(express.json());
 
-Blog.sync();
+app.use('/api/blogs', blogsRouter);
 
-app.get('/api/blogs', async (req, res) => {
-	const blogs = await Blog.findAll();
-	res.json(blogs);
-});
+const start = async () => {
+	await connectToDatabase();
+	app.listen(PORT, () => {
+		console.log(`Server running on port ${PORT}`);
+	});
+};
 
-app.post('/api/blogs/', async (req, res) => {
-	try {
-		const blog = await Blog.create(req.body);
-		res.json(blog);
-	} catch (error) {
-		return res.status(400).json({ error });
-	}
-});
-
-app.get('/api/blogs/:id', async (req, res) => {
-	const blog = await Blog.findByPk(req.params.id);
-	if (blog) {
-		res.json(blog);
-	} else {
-		res.status(404).end();
-	}
-});
-
-app.delete('/api/blogs/:id', async (req, res) => {
-	await Blog.findByPk(req.params.id).remove();
-});
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
-});
+start();
--- osa13/notes-storage-app/index.js
+++ osa13/notes-storage-app/index.js
@@ -1,72 +1,20 @@
-require('dotenv').config();
-const { Sequelize, Model, DataTypes } = require('sequelize');
 const express = require('express');
 const app = express();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-	dialectOptions: {
-		ssl: {
-			require: true,
-			rejectUnauthorized: false
-		}
-	}
-});
+const { PORT } = require('./utils/config');
+const { connectToDatabase } = require('./utils/db');
 
-class Note extends Model{}
+const notesRouter = require('./controllers/notes');
 
-Note.init({
-	id: {
-		type: DataTypes.INTEGER,
-		primaryKey: true,
-		autoIncrement: true
-	},
-	content: {
-		type: DataTypes.TEXT,
-		allowNull: false
-	},
-	important: {
-		type: DataTypes.BOOLEAN
-	},
-	date: {
-		type: DataTypes.DATE
-	}
-}, {
-	sequelize,
-	underscored: true,
-	timestamps: false,
-	modelName: 'note'
-});
+app.use(express.json());
 
-Note.sync();
+app.use('/api/notes', notesRouter);
 
-app.get('/api/notes/', async (req, res) => {
-	const notes = await Note.findAll();
+const start = async () => {
+	await connectToDatabase();
+	app.listen(PORT, () => {
+		console.log(`Server running on port ${PORT}`);
+	});
+};
 
-	console.log(JSON.stringify(notes, null, 2));
-
-	res.json(notes);
-});
-
-app.post('/api/notes/', async (req, res) => {
-	try {
-		const note = await Note.create(req.body);
-		res.json(note);
-	} catch (error) {
-		return res.status(400).json({ error });
-	}
-});
-
-app.get('/api/notes/:id', async (req, res) => {
-	const note = await Note.findByPk(req.params.id);
-	if (note) {
-		console.log(note.toJSON());
-		res.json(note);
-	} else {
-		res.status(404).end();
-	}
-});
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
-});
+start();
