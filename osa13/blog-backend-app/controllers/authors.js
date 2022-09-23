f457b770ce58d49d9881094258cc80b577c6e864
JojoSnow <milzwow94@gmail.com>
2022-09-23 10:53:07 +0300

    osa13b final 13.16 done

--- osa13/blog-backend-app/controllers/authors.js
+++ osa13/blog-backend-app/controllers/authors.js
@@ -12,7 +12,7 @@ router.get('/', async (req, res) => {
 			[sequelize.fn('sum', sequelize.col('likes')), 'likes']
 		],
 		order: [
-			[sequelize.fn('sum', sequelize.col('likes')), 'DESC']
+			['likes', 'DESC']
 		]
 		
 });
